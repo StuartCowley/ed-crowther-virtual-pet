807672f4da936516daed1dce118ae78f130bb78e
Ed Crowther <edward930@hotmail.co.uk>
2022-12-13 17:44:25 +0000

    initial age and grow up test and function works

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,10 +1,28 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-  
-    it('sets the name property', () => {
-      const pet = new Pet('Fido');
-  
-      expect(pet.name).toEqual('Fido');
+    it('returns an object', () => {
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-  });
+});
+describe('naming pet', () => {   
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.name).toEqual('Fido');
+    });
+});
+describe('initial age', () => {
+    it('has a initial age of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    });
+});
+describe('growUp', () => {
+    it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+    })
+})
+    
+    
